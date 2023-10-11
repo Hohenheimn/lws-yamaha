9a0a25e0fffcbe2d4bf0a191c8883b876502ccbe
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-10-12 00:59:04 +0800

    feat:init

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div>
+            <div>Unprotected Page</div>
+            <div>{children}</div></div>
+    )
+}
+
+export default AuthLayout
---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "@/styles/globals.scss"
+
+const RootLayout = ({
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout

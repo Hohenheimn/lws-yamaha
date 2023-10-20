7b8b0cf9c17603b9b48ee32012a155d606975a63
Jomari Tiu <85975045+Hohenheimn@users.noreply.github.com>
2023-10-20 10:58:40 +0800

    feat:screen banner

---
+++ src/components/ScreenBanner.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+type props = {
+  imageUrl: string;
+  setShow: Function;
+};
+
+function ScreenBanner({ imageUrl, setShow }: props) {
+  return (
+    <section className=" fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-[#00000043] z-50">
+      <aside className=" relative px-5">
+        <div className=" flex justify-end w-full">
+          <AiOutlineCloseCircle
+            onClick={() => setShow(false)}
+            className=" text-white text-cs-lg mb-2 cursor-pointer hover:text-gray-300 duration-150"
+          />
+        </div>
+        <Image
+          src={imageUrl}
+          alt="Banner"
+          width={1100}
+          height={500}
+          className=" object-contain"
+        />
+      </aside>
+    </section>
+  );
+}
+
+export default ScreenBanner;

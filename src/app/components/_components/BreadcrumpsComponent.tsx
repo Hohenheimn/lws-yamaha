0eebf7d69d472ace9db95724367fc73fa5419c1b
Marvin Ronquillo <marvin.r@lightweightsolutions.me>
2023-10-25 15:03:13 +0800

    feat :breadcrupms and product filter:

---
+++ src/app/components/_components/BreadcrumpsComponent.tsx
@@ -0,0 +1,11 @@
+import Breadcrumps from '@/components/partsAndAccessories/Breadcrumps'
+import breadcrumpsData from '@/data/partsAndAccessories/breadcrumps'
+import React from 'react'
+
+const BreadcrumpsComponent = () => {
+    return (
+        <Breadcrumps breadcrupms={breadcrumpsData} />
+    )
+}
+
+export default BreadcrumpsComponent
--- src/components/partsAndAccessories/ProductFilter/index.tsx
+++ src/components/partsAndAccessories/ProductFilter/index.tsx
@@ -1,68 +1,66 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MdOutlineCheckBox, MdOutlineCheckBoxOutlineBlank, MdArrowDropDown } from 'react-icons/md'
 
-type ProductCategoryType = {
-  id: number,
-  name: string,
-  subCategories?: ProductSubCategoryType[]
-}
-
-type ProductSubCategoryType = {
-  id: number,
-  name: string,
-}
-
-type FilterType = {
-  categoryId: number,
-  subCategoryIds: number[]
-}
-
 export type ProductFilterType = {
-  categories: ProductCategoryType[]
+  id: number,
+  name: string,
+  subCategories?: {
+    id: number,
+    name: string,
+  }[]
 }
 
+type PropsType = {
+  activeCategoryId?: number;
+  onClickCategory(categoryId: number): void;
+  categories: ProductFilterType[],
+  onChangeFilter(subCategoryIds: number[]): void
+}
 
-const ProductFilter = (props: ProductFilterType) => {
-  const [activeCategoryIds, setActiveCategoryIds] = useState<number[]>([])
-  const [filter, setFilter] = useState<FilterType[]>([])
+const ProductFilter = (props: PropsType) => {
+  const [subCategoryIds, setSubCategoryIds] = useState<number[]>([])
 
-  const addCategoryId = (id: number) => {
-    if (activeCategoryIds.includes(id)) {
-      const ids = activeCategoryIds.filter((categoryId) => categoryId !== id)
+  useEffect(() => {
+    setSubCategoryIds([])
+  }, [props.activeCategoryId])
 
-      return setActiveCategoryIds(ids)
-    }
+  useEffect(() => {
+    props.onChangeFilter(subCategoryIds)
+  }, [subCategoryIds])
 
-    return setActiveCategoryIds([...activeCategoryIds, id])
+  const addSubCategoryId = (subCategoryId: number) => {
+    setSubCategoryIds((prevState) => {
+      if (subCategoryIds.includes(subCategoryId)) {
+        return prevState.filter((id) => id !== subCategoryId)
+      }
+
+      return [...prevState, subCategoryId]
+    })
   }
 
-  const addFilter = (args: { categoryId: number, subCategoryId: number }) => {
-    // setFilter((prevState) => ))
-  }
-
-
-  const renderSubCategory = (args: { categoryId: number, subCategory: ProductSubCategoryType }) => {
-    return <div key={args.subCategory.id} className='flex gap-3 items-center'>
-      <MdOutlineCheckBox className="text-[#f1f1f1] w-5 h-5" onClick={() => addFilter({ categoryId: args.categoryId, subCategoryId: args.subCategory.id })} />
-      <span className='text-[#f1f1f1]'>{args.subCategory.name}</span>
+  const renderSubCategories = (subCategories: ProductFilterType['subCategories']) => {
+    return <div className={`pl-6 flex flex-col gap-3`}>
+      {subCategories?.map((subCategory) => (
+        <div key={subCategory.id} onClick={() => addSubCategoryId(subCategory.id)} className='flex cursor-pointer gap-3 items-center'>
+          {subCategoryIds.includes(subCategory.id) ? <MdOutlineCheckBox className="text-[#f1f1f1] w-5 h-5" /> : <MdOutlineCheckBoxOutlineBlank className="text-[#f1f1f1] w-5 h-5" />}
+          <span className='text-[#f1f1f1]'>{subCategory.name}</span>
+        </div>
+      ))}
     </div>
-  }
 
-  const renderCategory = (category: ProductCategoryType) => {
-    return <div key={category.id} className='flex flex-col gap-3'>
-      <div className='flex gap-1 items-center cursor-pointer' onClick={() => addCategoryId(category.id)}>
-        <MdArrowDropDown className="text-[#f1f1f1] w-6 h-6" />
-        <span className='text-[#f1f1f1] text-lg font-semibold'>{category.name}</span>
-      </div>
-      <div className={`pl-6 flex flex-col gap-3 ${!activeCategoryIds.includes(category.id) && 'hidden'}`}>
-        {category.subCategories?.map((subCategory) => renderSubCategory({ categoryId: category.id, subCategory }))}
-      </div>
-    </div>
   }
 
   return (
     <div className='bg-[#323232] rounded-lg px-4 py-6 flex flex-col gap-4 max-w-sm'>
-      {props.categories.map(renderCategory)}
+      {props.categories.map((category) => (
+        <div key={category.id} className='flex flex-col gap-3'>
+          <div className='flex gap-1 items-center cursor-pointer' onClick={() => props.onClickCategory(category.id)}>
+            <MdArrowDropDown className={`text-[#f1f1f1] w-6 h-6 transition-transform ${category.id === props.activeCategoryId && 'rotate-180'}`} />
+            <span className='text-[#f1f1f1] text-lg font-semibold'>{category.name}</span>
+          </div>
+          {props.activeCategoryId === category.id && renderSubCategories(category.subCategories)}
+        </div>
+      ))}
     </div>
   )
 }

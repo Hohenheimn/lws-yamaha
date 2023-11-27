b31a28129eb507a9d054af96cdbad7c61b16d3ec
Jomari Tiu <85975045+Hohenheimn@users.noreply.github.com>
2023-11-27 09:12:59 +0800

    fixes

--- src/components/sections/ImageAndParagraph/index.tsx
+++ src/components/sections/ImageAndParagraph/index.tsx
@@ -18,22 +18,22 @@ type PropsType = {
   contentAlignment: "left" | "right" | "center";
   content: {
     type: "text" | "list" | "icons";
-    value:
-      | string
-      | string[]
-      | {
-          icon: string;
-          title: string;
-        }[]
-      | {
-          description: string;
-          title: string;
-        }[];
+    value: string | string[] | IconsType[] | ListType[];
   }[];
   button?: {
     title: string;
     url: string;
   };
+};
+
+type ListType = {
+  description: string;
+  title: string;
+};
+
+type IconsType = {
+  icon: string;
+  title: string;
 };
 
 const ImageAndParagraph = (props: PropsType) => {
@@ -67,57 +67,74 @@ const ImageAndParagraph = (props: PropsType) => {
       >
         <Heading type="h5">{title}</Heading>
         {description && <p>{description}</p>}
-        {content.map((item: any, indx) => (
-          <aside key={indx}>
-            {item.type === "text" && typeof item.value === "string" && (
-              <p className=" text-[#A3A3A3]">{item.value}</p>
-            )}
-            {item.type === "list" && item.value[0]?.title && (
-              <ul>
-                {typeof item.value !== "string" && (
-                  <>
-                    {item.value.map((item: any, indx: number) => (
-                      <li key={indx} className=" py-5 border-b">
-                        {item.title}
-                        {item?.description && (
-                          <p className=" text-textGray">{item?.description}</p>
-                        )}
-                      </li>
-                    ))}
-                  </>
-                )}
-              </ul>
-            )}
-            {item.type === "icons" && item.value[0]?.icon && (
-              <>
-                {typeof item.value !== "string" && (
-                  <ul className=" flex gap-5 flex-wrap justify-center">
-                    {item.value.map((icon: any, indx: number) => (
-                      <li
-                        key={indx}
-                        className=" flex flex-col items-center justify-center"
-                      >
-                        {icon.icon && (
-                          <aside className=" w-20 aspect-square relative">
-                            <Image
-                              src={validateImageUrl(icon.icon)}
-                              alt="icon"
-                              fill
-                              sizes="(max-width: 768px) 5rem, (max-width: 1200px) 5rem, 5rem"
-                              className=" object-contain"
-                            />
-                          </aside>
-                        )}
+        {content.map(
+          (
+            item: { type: string; value: ListType[] | IconsType[] | any },
+            indx
+          ) => (
+            <aside key={indx}>
+              {item.type === "text" && typeof item.value === "string" && (
+                <p className=" text-[#A3A3A3]">{item.value}</p>
+              )}
+              {item.type === "list" && item.value[0]?.title && (
+                <ul>
+                  {typeof item.value !== "string" && (
+                    <>
+                      {item.value.map(
+                        (
+                          item: { title: string; description: string },
+                          indx: number
+                        ) => (
+                          <li key={indx} className=" py-5 border-b">
+                            {item.title}
+                            {item?.description && (
+                              <p className=" text-textGray">
+                                {item?.description}
+                              </p>
+                            )}
+                          </li>
+                        )
+                      )}
+                    </>
+                  )}
+                </ul>
+              )}
+              {item.type === "icons" && item.value[0]?.icon && (
+                <>
+                  {typeof item.value !== "string" && (
+                    <ul className=" flex gap-5 flex-wrap justify-center">
+                      {item.value.map(
+                        (
+                          icon: { icon: string; title: string },
+                          indx: number
+                        ) => (
+                          <li
+                            key={indx}
+                            className=" flex flex-col items-center justify-center"
+                          >
+                            {icon.icon && (
+                              <aside className=" w-20 aspect-square relative">
+                                <Image
+                                  src={validateImageUrl(icon.icon)}
+                                  alt="icon"
+                                  fill
+                                  sizes="(max-width: 768px) 5rem, (max-width: 1200px) 5rem, 5rem"
+                                  className=" object-contain"
+                                />
+                              </aside>
+                            )}
 
-                        {icon.title}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </>
-            )}
-          </aside>
-        ))}
+                            {icon.title}
+                          </li>
+                        )
+                      )}
+                    </ul>
+                  )}
+                </>
+              )}
+            </aside>
+          )
+        )}
         {button?.title && (
           <div className="flex justify-start">
             <Button appearance="primary" url={button?.url} size={"medium"}>

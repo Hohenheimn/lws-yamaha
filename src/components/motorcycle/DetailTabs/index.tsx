feee495fc83f08846ec3b04d5494375902666188
kennethpole6 <polekenneth11@gmail.com>
2023-12-06 17:29:48 +0800

    refactor(ui): detail tab gap in motorcycle

--- src/components/motorcycle/DetailTabs/index.tsx
+++ src/components/motorcycle/DetailTabs/index.tsx
@@ -14,10 +14,14 @@ const DetailTabs = (props: PropsType) => {
   const { tabs } = props;
 
   return (
-    <div className="flex justify-center w-full">
-      <div className="flex text-[#f1f1f1] text-lg gap-9 flex-wrap">
+    <div className="flex justify-center w-full p-2">
+      <div className="flex text-[#f1f1f1] text-lg gap-2 md:gap-9 flex-wrap justify-center">
         {tabs.map((tab, idx) => (
-          <button key={tab.id} onClick={tab.onClick} className="flex gap-9">
+          <button
+            key={tab.id}
+            onClick={tab.onClick}
+            className="flex gap-2 md:gap-9"
+          >
             <span className="hover:opacity-80">{tab.name}</span>
             {idx < tabs.length - 1 && "|"}
           </button>

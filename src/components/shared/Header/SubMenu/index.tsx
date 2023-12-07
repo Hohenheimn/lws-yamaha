1f5fec655b3d0e0a6fc7dd0011827b89f9654f20
kennethpole6 <polekenneth11@gmail.com>
2023-12-07 11:02:27 +0800

    refactor(ui): fixed nav height, hover links

--- src/components/shared/Header/SubMenu/index.tsx
+++ src/components/shared/Header/SubMenu/index.tsx
@@ -13,7 +13,7 @@ type props = {
 
 export default function SubMenu({ clickedMenu, setClickedMenu }: props) {
   return (
-    <nav className=" flex flex-col max-h-[92vh] overflow-auto md:px-0 justify-start items-center w-full absolute top-full left-0 pt-10 pb-3 bg-[#28282878] backdrop-blur-lg">
+    <nav className="flex flex-col max-h-[92vh] md:px-0 justify-start items-center w-full absolute top-full left-0 py-4 bg-[#28282890] backdrop-blur-lg md:h-80">
       {clickedMenu?.children[0].children && (
         <NavSubTab
           tabs={clickedMenu?.children}
@@ -28,11 +28,11 @@ export default function SubMenu({ clickedMenu, setClickedMenu }: props) {
       )}
 
       <aside
-        className=" cursor-pointer group group-hover flex items-center justify-center gap-2"
+        className="absolute bottom-1 left-1/2 transform -translate-x-1/2 cursor-pointer group group-hover flex items-center justify-center gap-1 mb-1"
         onClick={() => setClickedMenu(null)}
       >
-        <p>Close</p>
-        <IoMdCloseCircle className=" text-3xl text-gray-300 duration-150 group-hover:text-secondary-2" />
+        <p className="font-medium lg:text-sm">Close</p>
+        <IoMdCloseCircle className="text-3xl text-gray-300 duration-150 group-hover:text-secondary-2" />
       </aside>
     </nav>
   );

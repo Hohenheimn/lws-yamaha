5795f905b3d1e114acfc0513dbd74dc2d6c924f2
kennethpole6 <polekenneth11@gmail.com>
2023-12-14 11:09:13 +0800

    refactor: navigation button size

--- src/components/sections/MotorcycleCarouselSection/MotorcycleNavigationButton.tsx
+++ src/components/sections/MotorcycleCarouselSection/MotorcycleNavigationButton.tsx
@@ -7,15 +7,15 @@ export const MotorcycleNavigationButton = () => {
     <>
       <button
         onClick={() => swiper.slidePrev()}
-        className=" absolute top-[50%] left-0 translate-y-[-50%] z-10 w-[66px] aspect-square rounded-full bg-gray-300 hover:bg-gray-400 duration-150 flex justify-center items-center"
+        className=" absolute top-[50%] left-0 translate-y-[-50%] z-10 w-[60px] aspect-square rounded-full bg-gray-300 hover:bg-gray-400 duration-150 flex justify-center items-center"
       >
-        <BiSolidLeftArrow className="text-cs-xl" />
+        <BiSolidLeftArrow className="text-cs-lg" />
       </button>
       <button
         onClick={() => swiper.slideNext()}
-        className=" absolute top-[50%] right-0 translate-y-[-50%] z-10 w-[66px] aspect-square rounded-full bg-gray-300 hover:bg-gray-400 duration-150 flex justify-center items-center"
+        className=" absolute top-[50%] right-0 translate-y-[-50%] z-10 w-[60px] aspect-square rounded-full bg-gray-300 hover:bg-gray-400 duration-150 flex justify-center items-center"
       >
-        <BiSolidRightArrow className=" text-cs-xl" />
+        <BiSolidRightArrow className=" text-cs-lg" />
       </button>
     </>
   );

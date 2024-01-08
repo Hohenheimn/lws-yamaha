de619ec01b36d80a389a7917366a7d44f90a7f41
Jomari Tiu <85975045+Hohenheimn@users.noreply.github.com>
2024-01-08 14:37:47 +0800

    fix:yamaha studio

--- src/components/shared/EmbedSocials/GridEmbedSection/index.tsx
+++ src/components/shared/EmbedSocials/GridEmbedSection/index.tsx
@@ -2,7 +2,6 @@ import Script from "next/script";
 import { v4 } from "uuid";
 
 type PropsType = {
-  key?: string;
   embedId: string;
 };
 
@@ -15,7 +14,6 @@ const GridEmbedSection = (props: PropsType) => {
         dangerouslySetInnerHTML={{
           __html: `(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/cdn/ht.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialHashtagScript"))`,
         }}
-        strategy="beforeInteractive"
       />
       <div
         className="embedsocial-hashtag custom-embed"
